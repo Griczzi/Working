c8a7fcce3cccdde9921a791a7c30d0eb59aedde8
Ilia Hryts <Griczi@mail.ru>
2020-10-11 01:31:03 +0200

    New variables

--- script/script.js
+++ script/script.js
@@ -1,9 +1,22 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 330;
+let income = 200;
+let addExpenses = "Такси: 20, Комуналка: 80, Бар: 10";
+let deposit = true;
+let mission = 10000;
+let period = 12;
+let budgetDay = money / 20;
 
-alert('hello world!');
-console.log('hello my friend!');
+console.log(typeof (deposit));
+console.log(typeof (income));
+console.log(typeof (money));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toUpperCase());
+console.log(addExpenses.split(''));
+console.log('budgetDay: ', budgetDay);
+
+
+
+// - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+// - Вывести в консоль budgetDay
